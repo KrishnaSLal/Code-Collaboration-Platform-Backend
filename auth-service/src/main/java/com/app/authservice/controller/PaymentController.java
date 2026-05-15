0e1d55bb1444a38fc317c6a525b30e5a1708391a
KrishnaSLal <krishnaslal0204@gmail.com>
2026-05-15 10:19:00 +0530

    [Krishna]: Modified the controller service

--- auth-service/src/main/java/com/app/authservice/controller/PaymentController.java
+++ auth-service/src/main/java/com/app/authservice/controller/PaymentController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/payments")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:4200")
 public class PaymentController {
 
     private final PaymentService paymentService;
